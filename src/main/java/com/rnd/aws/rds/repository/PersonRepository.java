61f6f213c02c21ad853934e68d60909f8d9750b8
Fahim Fahad <fahim.fahad@toptal.com>
2021-10-02 13:35:37 +0600

    basic setup

--- src/main/java/com/rnd/aws/rds/repository/PersonRepository.java
+++
@@ -1,8 +0,0 @@
-package com.rnd.aws.rds.repository;
-
-import com.rnd.aws.rds.entity.Person;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PersonRepository extends JpaRepository<Person, Long> {}
