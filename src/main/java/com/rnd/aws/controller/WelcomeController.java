8ec38664b87d2f24df0886c024240efd2e02cc56
Fahim Fahad <fahim.fahad@toptal.com>
2022-01-09 14:07:02 +0600

    add swagger configuration

--- src/main/java/com/rnd/aws/controller/WelcomeController.java
+++ src/main/java/com/rnd/aws/controller/WelcomeController.java
@@ -6,11 +6,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 /*
 Fahim created at 3/3/2021
@@ -21,12 +20,12 @@ public class WelcomeController {
 
   private static final Logger LOG = LoggerFactory.getLogger(WelcomeController.class);
 
+  List<String> values = new ArrayList<>();
+
   @GetMapping(value = "/index")
   public ResponseEntity<ServiceResponse> index() {
 
     ServiceResponse serviceResponse = new ServiceResponse();
-
-    log();
 
     serviceResponse.setBody("Welcome to AWS RnD Java Project");
     serviceResponse.setStatus(HttpStatus.OK);
@@ -35,16 +34,43 @@ public class WelcomeController {
     return ResponseEntity.ok(serviceResponse);
   }
 
-  private void log() {
-    LOG.info("This is a log test at time " + LocalDateTime.now());
-    for (int i = 0; i < 10; i++) {
-      LOG.info("The number i = " + i);
-    }
+  @PostMapping(value = "/")
+  public ResponseEntity<ServiceResponse> addValue(@RequestBody String value) {
 
-    try {
-      throw new RuntimeException("Some runtime error");
-    } catch (Exception e) {
-      LOG.error(e.getMessage(), e);
-    }
+    this.values.add(value);
+    ServiceResponse serviceResponse = new ServiceResponse();
+
+
+    serviceResponse.setBody("value added to list");
+    serviceResponse.setStatus(HttpStatus.OK);
+    serviceResponse.setStatusCode(StatusCode.SUCCESS);
+
+    return ResponseEntity.ok(serviceResponse);
+  }
+
+  @DeleteMapping(value = "/")
+  public ResponseEntity<ServiceResponse> deleteValue(@RequestBody String value) {
+
+    this.values.removeIf(v->v.equals(value));
+    ServiceResponse serviceResponse = new ServiceResponse();
+
+
+    serviceResponse.setBody("value deleted from list");
+    serviceResponse.setStatus(HttpStatus.OK);
+    serviceResponse.setStatusCode(StatusCode.SUCCESS);
+
+    return ResponseEntity.ok(serviceResponse);
+  }
+
+  @GetMapping(value = "/")
+  public ResponseEntity<ServiceResponse> getValue() {
+
+    ServiceResponse serviceResponse = new ServiceResponse();
+
+    serviceResponse.setBody(values);
+    serviceResponse.setStatus(HttpStatus.OK);
+    serviceResponse.setStatusCode(StatusCode.SUCCESS);
+
+    return ResponseEntity.ok(serviceResponse);
   }
 }
