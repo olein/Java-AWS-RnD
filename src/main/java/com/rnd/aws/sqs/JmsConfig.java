07b2ff20d012720694960c03f61b80769e883890
Fahim Fahad <fahim.fahad@toptal.com>
2022-02-09 12:04:12 +0600

    Improve sqs connection factory configuration

--- src/main/java/com/rnd/aws/sqs/JmsConfig.java
+++ src/main/java/com/rnd/aws/sqs/JmsConfig.java
@@ -2,16 +2,22 @@ package com.rnd.aws.sqs;
 
 import com.amazon.sqs.javamessaging.ProviderConfiguration;
 import com.amazon.sqs.javamessaging.SQSConnectionFactory;
+import com.amazonaws.auth.DefaultAWSCredentialsProviderChain;
+import com.amazonaws.regions.Region;
+import com.amazonaws.regions.Regions;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jms.annotation.EnableJms;
 import org.springframework.jms.config.DefaultJmsListenerContainerFactory;
+import org.springframework.jms.connection.CachingConnectionFactory;
 import org.springframework.jms.core.JmsTemplate;
 import org.springframework.jms.support.destination.DynamicDestinationResolver;
 
 import javax.annotation.PostConstruct;
 import javax.jms.Session;
+
+import static org.springframework.jms.listener.DefaultMessageListenerContainer.CACHE_AUTO;
 
 @Configuration
 @EnableJms
@@ -20,20 +26,42 @@ public class JmsConfig {
   @Autowired AmazonSqsClient amazonSQSClient;
   private SQSConnectionFactory connectionFactory;
 
-  @PostConstruct
-  public void init() {
-    ProviderConfiguration providerConfiguration = new ProviderConfiguration();
-    connectionFactory =
-        new SQSConnectionFactory(providerConfiguration, amazonSQSClient.getClient());
-  }
+//  @PostConstruct
+//  public void init() {
+//    ProviderConfiguration providerConfiguration = new ProviderConfiguration();
+//    connectionFactory =
+//        new SQSConnectionFactory(providerConfiguration, amazonSQSClient.getClient());
+//  }
+
+//  @Bean
+//  public DefaultJmsListenerContainerFactory jmsListenerContainerFactory() {
+//    DefaultJmsListenerContainerFactory factory = new DefaultJmsListenerContainerFactory();
+//    factory.setConnectionFactory(this.connectionFactory);
+//    factory.setDestinationResolver(new DynamicDestinationResolver());
+//    factory.setConcurrency("3-10");
+//    factory.setSessionAcknowledgeMode(Session.CLIENT_ACKNOWLEDGE);
+//    return factory;
+//  }
 
   @Bean
   public DefaultJmsListenerContainerFactory jmsListenerContainerFactory() {
+    this.connectionFactory =
+            SQSConnectionFactory.builder()
+                    .withRegion(Region.getRegion(Regions.US_EAST_1))
+                    .withAWSCredentialsProvider(new DefaultAWSCredentialsProviderChain())
+                    .withNumberOfMessagesToPrefetch(0)
+                    .build();
+
+    CachingConnectionFactory cachingConnectionFactory = new CachingConnectionFactory();
+    cachingConnectionFactory.setTargetConnectionFactory(this.connectionFactory);
+    cachingConnectionFactory.setReconnectOnException(true);
+
     DefaultJmsListenerContainerFactory factory = new DefaultJmsListenerContainerFactory();
     factory.setConnectionFactory(this.connectionFactory);
     factory.setDestinationResolver(new DynamicDestinationResolver());
     factory.setConcurrency("3-10");
-    factory.setSessionAcknowledgeMode(Session.CLIENT_ACKNOWLEDGE);
+    factory.setSessionAcknowledgeMode(Session.AUTO_ACKNOWLEDGE);
+    factory.setCacheLevel(CACHE_AUTO);
     return factory;
   }
 
